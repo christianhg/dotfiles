61c83a2a66c8ae241cde1f31f9fece43bcddff41
Christian Hamburger Grøngaard <christianhg@gmail.com>
2017-12-04 14:34:48 +0100

    Add script to add python, pip and pyyaml

---
+++ python.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+brew install python
+
+# This formula installs a python2 executable to /usr/local/bin.
+# If you wish to have this formula's python executable in your PATH then add
+# the following to ~/.zshrc:
+#   export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+
+# Pip and setuptools have been installed. To update them
+#   pip2 install --upgrade pip setuptools
+
+# You can install Python packages with
+#   pip2 install <package>
+
+pip2 install pyyaml
