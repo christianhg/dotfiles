2b2dd0a2dd86da2d1705094b9d59c5864913436d
Christian Hamburger Grøngaard <christianhg@gmail.com>
2018-11-15 15:10:34 +0100

    Automatically add symlink to .jira-url

--- zsh.sh
+++ zsh.sh
@@ -5,4 +5,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+ln -s .jira-url ~/.jira-url
+
 npm install --global pure-prompt
