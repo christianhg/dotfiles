16092fdb47f6125493821490de13e8803f57dd37
Christian Hamburger Grøngaard <christianhg@gmail.com>
2018-11-15 20:10:47 +0100

    Add jq

--- scripts.sh
+++ scripts.sh
@@ -3,3 +3,6 @@ git clone https://gitlab.com/skybert/my-little-friends.git ~/code/my-little-frie
 ln -s ~/code/my-little-friends/bash/log-work-on-jira-issue /usr/local/bin/log-work-on-jira-issue
 
 touch ~/.log-work-on-jira-issue.conf
+
+# JSON processor required by log-work-on-jira-issue
+brew install jq
