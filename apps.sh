cdf49786aa1df321e5b3c68f88c1a26698940411
Christian Hamburger Grøngaard <christian@groengaard.dk>
2022-08-28 15:11:03 +0200

    Use Homebrew Bash installer

--- apps.sh
+++ apps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Make sure we’re using the latest Homebrew.
 brew update
