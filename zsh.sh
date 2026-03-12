e6160dc9a0b525732b1ff95e75bf84c8a64872d0
Christian Grøngaard <christian@groengaard.dk>
2026-03-12 19:13:20 +0100

    fix: use if/then instead of && to avoid non-zero exit code
    The [ ! -d ... ] && git clone pattern returns exit code 1 when
    the directory already exists, which causes set -e in setup.sh
    to abort the entire script.

--- zsh.sh
+++ zsh.sh
@@ -7,5 +7,11 @@ fi
 
 # Install plugins into oh-my-zsh custom directory (skip if already cloned)
 ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
-[ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
-[ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ] && git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+fi
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+fi
